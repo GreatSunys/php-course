300e9e2afbc46fb57ea56a523fe40870b35edffc
Greatsunys <gsunysmail@tuta.io>
2021-03-17 01:13:50 +0100

    add variables

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     $name = "PHP Store";
     $credit = 1000;
 
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
+    
     echo "<h1>Welcome!</h1>";
     echo "<h2>You have $".$credit." dans votre wallet.</h2>"
     ?>
